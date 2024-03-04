f7b28ac038a0a8601f978d8c27d9ccf0f0aba7b6
Juan Pablo <juanpa_oso@hotmail.com>
2024-03-03 20:14:41 -0500

    codigo con el switch falta punto 5

---
+++ _taller_29feb/interpolacion_importante_original.py
@@ -0,0 +1,68 @@
+#En este ejercicio se le dan directamente los valores que se quieren evaluar en el polinomio
+# El polinomio de interpolación
+import numpy as np
+import sympy as sym #Para crear la expresion del polinomio
+import matplotlib.pyplot as plt
+
+# Ingreso de datos, pero acá están presentados como listas
+
+def interpolacion_oroginal(x,y):
+
+  # Procedimiento, pasamos a convertirlos en arreglos
+  xi = np.array(x)
+  fi = np.array(y)
+  B = np.copy (y)
+
+  #Matriz
+  n = len(xi) #Tamaño de la matriz
+  D = np.zeros((n,n),dtype=float) #Matriz, llena de ceros
+  ultima = n-1
+
+  #Para llenar las casillas
+  i = 0 #Fila cero
+  for i in range(0,n,1): #mover el valor de i, en el rango desde la primera hasta la ultima
+    for j in range(0,n,1): #mover el valor de j, en el rango desde la primera hasta la ultima
+      potencia = ultima -j #Se calcula el valor de la exponente
+      D[i,j] = xi[i] **potencia #Posicion en la matriz
+
+  # Calculo de coeficientes del polinomio,mediante algebra lineal
+  coeficientes = np.linalg.solve(D,B)
+
+  #Polinomio de Interpolación
+  x = sym.Symbol('x') # X va a ser tomada como un simbolo
+  polinomio = 0 #Comenzamos con polinomio vacío
+  for i in range(0,n,1):
+    potencia = (n-1)-i #Es relativo al valor de la ultima posición
+    termino = coeficientes[i]*(x**potencia)
+    polinomio = polinomio + termino
+
+  #Para facilitar la evaluación del polinomio
+  px = sym.lambdify(x,polinomio)
+
+  #Evaluar polinomio
+  muestras = 51
+  a = np.min(xi) #Minimo
+  b= np.max(xi) #Máximo
+  pxi = np.linspace(a,b,muestras) #Serie de puntos muestreados
+  pfi = px(pxi) #puntos de la función,usando la forma numerica del polinomio
+
+
+  #Salida
+  print ('Matriz')
+  print ('D')
+  print ('Coeficientes:')
+  print (coeficientes)
+  print ('polinomio: ')
+  print (polinomio)
+  print (px(1.5))
+  print (px(5.7))
+  #crearemos una grafica
+  plt.plot(xi,fi,'o', label='puntos')
+  plt.plot(pxi,pfi, label='polinomio') #Trazamos la linea de los puntos
+  plt.legend() #Mostrar todas las etiquetas
+  plt.xlabel('xi') #Añadimos una etiqueta
+  plt.ylabel('fi') #Añadimos una etiqueta
+  plt.title('Polinomio de Interpolación')#Añadimos un titulo
+  plt.show() #Para ver la gráfica
+
+  
--- _taller_29feb/main.py
+++ _taller_29feb/main.py
@@ -3,19 +3,63 @@ import sympy as sym #Para crear la expresion del polinomio
 import matplotlib.pyplot as plt
 import interpolacion_importante as inti
 import inter_RBFMN as rbf
-
-
-
+import interpolacion_importante_original as init_original
+import inter_RBFMN_Origina as rbf_original
 
 datos= np.loadtxt('_taller_29feb\datos_importantes.txt')
-
+terminar = 0
 xi = datos[:,0]
 fi = datos[:,1]
 ultimo_dato = xi[-1]
 
+while(terminar == 0):
+    hacer = int(input("Que quieres hacer? \n 1:Metodo normal \n 2: metodo RBFMN \n 3: Union de los dos metodos \n 4: punto 5 \n 5: salir \n ingrese el numero: "))
+
+    if(hacer == 1):
+        init_original.interpolacion_oroginal(xi,fi)
+    elif(hacer == 2):
+        rbf_original.rbf_interpolacion(xi,fi)
+    elif(hacer == 3):
+        puntosx_inti,puntosy_inti,polinomiox_inti,polinomioy_inti = inti.interpolacion(xi,fi)
+        inicio = 0.4
+        fin = ultimo_dato+1
+        cantidad_numeros_deseados = len(polinomiox_inti)
+        paso = (fin - inicio) / (cantidad_numeros_deseados - 1)
+        x = np.arange(inicio, fin + paso, paso)
+
+        print(np.log(x))
+
+        Err = np.sqrt(np.sum((polinomioy_inti - np.log(x))**2)/len(polinomioy_inti))
+        print("error de nuestro polinomio", Err)
+
+        funcionx,funciony,interx,intery = rbf.rbf_interpolacion(xi,fi)
 
 
-puntosx_inti,puntosy_inti,polinomiox_inti,polinomioy_inti = inti.interpolacion(xi,fi)
+        plt.plot(funcionx, funciony, label = 'Funcion dada')
+        plt.plot(interx,intery, label = 'Interpolacion RBF')
+        plt.plot(xi,fi , 'or' , label = 'datos')
+        plt.plot(puntosx_inti,puntosy_inti,'o', label='puntos')
+        plt.plot(polinomiox_inti,polinomioy_inti, label='polinomio') #Trazamos la linea de los puntos
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.grid(True)
+        plt.legend() #Mostrar todas las etiquetas
+        plt.title('interpolacion con funciones de base radial')
+        plt.show() #Para ver la gráfica
+
+    elif(hacer == 5):
+        terminar = 1
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -23,56 +67,15 @@ puntosx_inti,puntosy_inti,polinomiox_inti,polinomioy_inti = inti.interpolacion(x
 # plt.xlabel('xi') #Añadimos una etiqueta
 # plt.ylabel('fi') #Añadimos una etiqueta
 
-inicio = 0.4
-fin = ultimo_dato+1
-cantidad_numeros_deseados = len(polinomiox_inti)
-paso = (fin - inicio) / (cantidad_numeros_deseados - 1)
-x = np.arange(inicio, fin + paso, paso)
-
-
-print(np.log(x))
-
-Err = np.sqrt(np.sum((polinomioy_inti - np.log(x))**2)/len(polinomioy_inti))
-print("error de nuestro polinomio", Err)
-
-funcionx,funciony,interx,intery = rbf.rbf_interpolacion(xi,fi)
-
-
-plt.plot(funcionx, funciony, label = 'Funcion dada')
-plt.plot(interx,intery, label = 'Interpolacion RBF')
-plt.plot(xi,fi , 'or' , label = 'datos')
-plt.plot(puntosx_inti,puntosy_inti,'o', label='puntos')
-plt.plot(polinomiox_inti,polinomioy_inti, label='polinomio') #Trazamos la linea de los puntos
-plt.xlabel('x')
-plt.ylabel('y')
-plt.grid(True)
-plt.legend() #Mostrar todas las etiquetas
-plt.title('interpolacion con funciones de base radial')
 
 
 
-plt.show() #Para ver la gráfica
-
-#Quinto punto del taller de interpolación 
-
-# Definir los puntos x
 
 
 
-# plt.xlabel('xi') #Añadimos una etiqueta
-# plt.ylabel('fi') #Añadimos una etiqueta
-
-inicio = 0.4
-fin = ultimo_dato+1
-cantidad_numeros_deseados = len(polinomiox_inti)
-paso = (fin - inicio) / (cantidad_numeros_deseados - 1)
-x = np.arange(inicio, fin + paso, paso)
 
 
-print(np.log(x))
 
-Err = np.sqrt(np.sum((polinomioy_inti - np.log(x))**2)/len(polinomioy_inti))
-print("error de nuestro polinomio", Err)
 
 
 
